e2ada8444c348d9a065159071a401b0323ef8301
asdfii <asdfii@126.com>
2017-04-17 18:58:56 +0800

    'Daily'

--- views.py
+++ views.py
@@ -17,9 +17,19 @@ from models import GitUser, GitGroup, GitRepo
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    pass
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(
+            request.form['username'],
+            request.form['password']
+            ):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
+
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -27,10 +37,21 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/tmp/upload/uploaded_file.txt')
+
+
 with app.test_request_context():
-    print url_for('index')
+    print url_for('login')
     print url_for('hello', name='test')
     print url_for('static', filename='./react/react.min.js')
+
+with app.test_request_context('/hello', method='POST'):
+    assert request.path == '/hello'
+    assert request.method == 'POST'
 
 if __name__ == '__main__':
     app.run()
