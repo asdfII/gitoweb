714ab5ffabb881d9a1e2967b64e2b249d0e1d566
asdfii <asdfii@126.com>
2017-04-26 17:49:35 +0800

    Add init_db_sqlite.py and modify some files.

--- create_gitconf.py
+++ create_gitconf.py
@@ -6,7 +6,7 @@ from gitoweb.settings import BASE_DIR
 
 
 with open(BASE_DIR + '/conf/gitolite.conf', 'rb') as file:
-    fp = open(BASE_DIR + '/conf/new.conf', 'ab+')
+    fp = open(BASE_DIR + '/new.conf', 'ab+')
     for line in file.readlines():
         #~ line = line.rstrip('\n')
         fp.write(line)
