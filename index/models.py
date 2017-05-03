e8da4f4853438fef3118dd04be31d75161dffe9c
asdfii <asdfii@126.com>
2017-05-03 15:23:24 +0800

    Modify index/models.py

--- index/models.py
+++ index/models.py
@@ -1,37 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import (
-    create_engine,
-    Column, CHAR, Integer, String,
+    Column, CHAR, Integer, String, ForeignKey,
     exc
 )
+from sqlalchemy.orm import relationship, backref
 from db.database import Base
-
-
-class GitUser(Base):
-    __tablename__ = 'git_user'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    
-    def __init__(self, id=None, name=None):
-        self.id = id
-        self.name = name
-    
-    def __unicode__(self):
-        return '<GitUser %s>' % (self.name)
-
-
-class GitGroup(Base):
-    __tablename__ = 'git_group'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    
-    def __init__(self, id=None, name=None):
-        self.id = id
-        self.name = name
-    
-    def __unicode__(self):
-        return '<GitGroup %s>' % (self.name)
 
 
 class GitRepo(Base):
@@ -45,3 +19,39 @@ class GitRepo(Base):
     
     def __unicode__(self):
         return '<GitRepo %s>' % (self.name)
+
+
+class GitGroup(Base):
+    __tablename__ = 'git_group'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    
+    git_repo_id = Column(Integer, ForeignKey('git_repo.id'))
+    git_repo = relationship('GitGroup', backref=
+        backref('git_group', lazy='dynamic')
+    )
+    
+    def __init__(self, id=None, name=None):
+        self.id = id
+        self.name = name
+    
+    def __unicode__(self):
+        return '<GitGroup %s>' % (self.name)
+
+
+class GitUser(Base):
+    __tablename__ = 'git_user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    
+    git_group_id = Column(Integer, ForeignKey('git_group.id'))
+    git_group = relationship('GitUser', backref=
+        backref('git_user', lazy='dynamic')
+    )
+    
+    def __init__(self, id=None, name=None):
+        self.id = id
+        self.name = name
+    
+    def __unicode__(self):
+        return '<GitUser %s>' % (self.name)
