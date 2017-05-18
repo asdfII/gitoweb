b67fb85797fb24202b887a39312b8eaf9ba376c9
AlphaZ <asdfii@126.com>
2017-05-19 00:57:11 +0800

    Ticket alphaz 20170519005711

--- static/assets/js/myjs.js
+++ static/assets/js/myjs.js
@@ -4,6 +4,24 @@ function updatePackage() {
     });
 }
 
+function selectAllUser(src) {
+    var checkboxes = document.getElementsByTagName('input');
+    if (src.checked) {
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].name == 'selectUserName') {
+                checkboxes[i].checked = true;
+            }
+        }
+    } else {
+        for (var i = 0; i < checkboxes.length; i++) {
+            console.log(i)
+            if (checkboxes[i].name == 'selectUserName') {
+                checkboxes[i].checked = false;
+            }
+        }
+    }
+}
+
 function selectAllGroup(src) {
     var checkboxes = document.getElementsByTagName('input');
     if (src.checked) {
