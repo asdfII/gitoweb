8d02f9dafa28f68c645138b7a61e57b1ad9fdcaf
asdfii <asdfii@126.com>
2017-05-19 18:43:42 +0800

    Ticket Administrator@MYTEST 20170519084342 win_push_dev.bat

--- win_push_dev.bat
+++ win_push_dev.bat
@@ -1,5 +1,8 @@
 @echo off
 git add -A
 set gitdiff=git diff --cached --name-only
-git commit -m "Ticket %username%@%computername% %date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2% %gitdiff%"
+%gitdiff% >>win_gitdiff.tmp
+set /p readdiff=<win_gitdiff.tmp
+del /q /f win_gitdiff.tmp
+git commit -m "Ticket %username%@%computername% %date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2% %readdiff%"
 git push origin dev
